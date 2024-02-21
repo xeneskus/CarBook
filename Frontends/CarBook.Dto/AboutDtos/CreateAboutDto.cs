781909b9b7ef3c51bcbe13f4b042ecf35802b5d9
xeneskus <xeneskus@gmail.com>
2024-02-21 18:54:32 +0300

    Added about and some view component settings

---
+++ Frontends/CarBook.Dto/AboutDtos/CreateAboutDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CarBook.Dto.AboutDtos
+{
+    public class CreateAboutDto
+    {
+        public string title { get; set; }
+        public string description { get; set; }
+        public string imageUrl { get; set; }
+    }
+}
---
+++ Frontends/CarBook.Dto/AboutDtos/ResultAboutDto.cs
@@ -0,0 +1,11 @@
+﻿namespace CarBook.Dto.AboutDtos
+{
+    public class ResultAboutDto
+    {
+        public int aboutID { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public string imageUrl { get; set; }
+
+    }
+}
