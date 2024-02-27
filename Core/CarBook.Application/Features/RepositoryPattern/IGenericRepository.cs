f069a54a78fb5fb051276bb0db9bfef48df91296
xeneskus <xeneskus@gmail.com>
2024-02-27 13:59:35 +0300

    Added comments api controller

---
+++ Core/CarBook.Application/Features/RepositoryPattern/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace CarBook.Application.Features.RepositoryPattern
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        List<T> GetAll();
+        void Create(T entity);
+        void Update(T entity);
+        void Remove(T entity);
+        T GetById(int id);
+        List<T> GetCommentsByBlogId(int id);
+        public int GetCountCommentByBlog(int id);
+    }
+}
--- Core/CarBook.Domain/Entities/Blog.cs
+++ Core/CarBook.Domain/Entities/Blog.cs
@@ -12,6 +12,7 @@
         public Category Category { get; set; }
         public string Description { get; set; }
         public List<TagCloud> TagClouds { get; set; }
+        public List<Comment> Comments { get; set; }
 
     }
 }
