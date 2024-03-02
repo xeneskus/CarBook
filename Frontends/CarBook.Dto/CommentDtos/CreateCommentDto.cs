04d5960ee073833cbac8991d21451dc9051192c7
xeneskus <xeneskus@gmail.com>
2024-03-02 21:45:52 +0300

    Added AdminCommentController and AdminBlogController in Area Section

---
+++ Frontends/CarBook.Dto/CommentDtos/CreateCommentDto.cs
@@ -0,0 +1,11 @@
+﻿namespace CarBook.Dto.CommentDtos
+{
+    public class CreateCommentDto
+    {
+        public string Name { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string Description { get; set; }
+        public int BlogID { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ Frontends/CarBook.Dto/CommentDtos/ResultCommentDto.cs
@@ -0,0 +1,11 @@
+﻿namespace CarBook.Dto.CommentDtos
+{
+    public class ResultCommentDto
+    {
+        public int CommentID { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string Description { get; set; }
+        public int BlogID { get; set; }
+    }
+}
