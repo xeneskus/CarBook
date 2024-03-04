538f852da71906e9744b51624a64b1cf684e688f
xeneskus <xeneskus@gmail.com>
2024-03-04 12:27:30 +0300

    Added AdminSocialMediaController

---
+++ Frontends/CarBook.Dto/SocialMediaDtos/ResultSocialMediaDto.cs
@@ -0,0 +1,10 @@
+﻿namespace CarBook.Dto.SocialMediaDtos
+{
+    public class ResultSocialMediaDto
+    {
+        public int SocialMediaID { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public string Icon { get; set; }
+    }
+}
