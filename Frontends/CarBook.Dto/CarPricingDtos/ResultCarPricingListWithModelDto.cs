01fde5babea0ecd26f63a2418f7d0f7252caa0c8
xeneskus <xeneskus@gmail.com>
2024-03-13 17:42:32 +0300

    Added CarPricingController and Index for MVC

---
+++ Frontends/CarBook.Dto/CarPricingDtos/ResultCarPricingListWithModelDto.cs
@@ -0,0 +1,13 @@
+﻿namespace CarBook.Dto.CarPricingDtos
+{
+    public class ResultCarPricingListWithModelDto
+	{
+		public string model { get; set; }
+		public decimal dailyAmount { get; set; }
+		public decimal weeklyAmount { get; set; }
+		public decimal monthlyAmount { get; set; }
+		public string CoverImageUrl { get; set; }
+		public string Brand { get; set; }
+	}
+
+}
