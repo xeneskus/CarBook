d8c1191b296e6b044f76676d0c36322c9bd318a7
xeneskus <xeneskus@gmail.com>
2024-03-18 23:36:49 +0300

    Added CarDetailViewComponents

---
+++ Frontends/CarBook.Dto/CarDescriptionDtos/ResultCarDescriptionByCarIdDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CarBook.Dto.CarDescriptionDtos
+{
+    public class ResultCarDescriptionByCarIdDto
+    {
+        public int CarDescriptionID { get; set; }
+        public int CarID { get; set; }
+        public string Details { get; set; }
+    }
+}
