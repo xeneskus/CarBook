3024a56da18ae84ee84f90b42202b3304fbe6daf
xeneskus <xeneskus@gmail.com>
2024-03-19 16:28:55 +0300

    Added Review Entity and Dto

--- Infrastructure/CarBook.Persistence/Context/CarBookContext.cs
+++ Infrastructure/CarBook.Persistence/Context/CarBookContext.cs
@@ -34,6 +34,7 @@ public class CarBookContext : DbContext
     public DbSet<RentACarProcess> RentACarProcesses { get; set; }
     public DbSet<Customer> Customers { get; set; }
     public DbSet<Reservation> Reservations { get; set; }
+    public DbSet<Review> Reviews { get; set; }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Reservation>()
