87eaddf9e53917085360466715cc780a64907361
xeneskus <xeneskus@gmail.com>
2024-03-21 00:07:19 +0300

    Added RegisterController for mvc

---
+++ Frontends/CarBook.Dto/RegisterDtos/CreateRegisterDto.cs
@@ -0,0 +1,11 @@
+﻿namespace CarBook.Dto.RegisterDtos
+{
+    public class CreateRegisterDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+    }
+}
