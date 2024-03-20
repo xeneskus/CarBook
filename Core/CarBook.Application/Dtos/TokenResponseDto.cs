49b81b64816b465a1cdb9a462e84458fb1165786
xeneskus <xeneskus@gmail.com>
2024-03-20 17:34:53 +0300

    Added TokenResponseDto

---
+++ Core/CarBook.Application/Dtos/TokenResponseDto.cs
@@ -0,0 +1,14 @@
+﻿namespace CarBook.Application.Dtos
+{
+    public class TokenResponseDto
+    {
+        public TokenResponseDto(string token, DateTime expireDate)
+        {
+            Token = token;
+            ExpireDate = expireDate;
+        }
+
+        public string Token { get; set; }
+        public DateTime ExpireDate { get; set; }
+    }
+}
